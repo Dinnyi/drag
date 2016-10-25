26695b07c6eb42556806774b4eb532aa748dbb74
cnige <211989491@qq.com>
2016-10-25 23:11:33 +0800

    success drag

--- drag.js
+++ drag.js
@@ -1,8 +1,20 @@
-/**
- * Created by LYH on 2016/10/25.
- */
-function drag(id) {
-    var obj=document.getElementById(id);
-    var disX=0;
-    var disY=0;
+// JavaScript Document
+
+function drag(id){
+	var obj = document.getElementById(id);
+	var disX = 0;
+	var disY = 0;
+	obj.onmousedown = function(ev){
+		disX = ev.pageX - obj.offsetLeft;
+		disY = ev.pageY - obj.offsetTop;
+		document.onmousemove = function(ev){
+			obj.style.left = ev.pageX - disX + 'px';
+			obj.style.top = ev.pageY - disY + 'px';
+		};
+		document.onmouseup = function(){
+			document.onmousemove = null;
+			document.onmouseup = null;
+		};
+	}
+	return false;
 }
